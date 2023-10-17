9c485c4ecb1b01a685ac42451e3f7da3800ed6ad
matthewpourroy <Pourroy.matt@gmail.com>
2023-10-17 11:14:15 -0400

    adjust cors

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ const app = express()
 const cors = require('cors')
 
 // middleware
-app.use(
-  cors({
-    origin: [
-      "https://workout-buddy-backend-chyp.onrender.com", 
-      "http://localhost:3000",
-      "https://workout-buddy-tzli.onrender.com"
-    ] 
-  }))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://workout-buddy-backend-chyp.onrender.com, http://localhost:3000, https://workout-buddy-tzli.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json())
 
 app.use((req, res, next) => {
